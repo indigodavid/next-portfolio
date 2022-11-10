17bf0ffb3018035d0bbe987a3aee91ed1e95e072
David Vera Castillo <indigodavid@gmail.com>
2022-11-10 16:57:56 -0500

    🎛 Configure images source in from ImageKitIo

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.imagekit.io',
+        pathname: '/ezcar/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
