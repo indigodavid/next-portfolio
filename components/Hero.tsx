d03b5160f4aaa70cfdb7d087f93ef3058511375e
David Vera Castillo <indigodavid@gmail.com>
2022-11-10 23:44:20 -0500

    ✅ Add navigation buttons and fix layering

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Image from 'next/image';
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 import BackgroundCircles from './BackgroundCircles';
+import Link from 'next/link';
 
 type Props = {}
 
 const Hero = (props: Props) => {
   const [text, count] = useTypewriter({
     words: [
-      "Hi myName=\"David\"",
+      "Hi myName={<David />}",
       "Passions are={ learning && coding }",
       "Enjoying life={maxValue} remoteWork={theBest}",
       "Hobbies instruments=\"guitar bass\"",
@@ -17,7 +18,7 @@ const Hero = (props: Props) => {
     delaySpeed: 2000,
   });
   return (
-    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden z-20">
       <BackgroundCircles />
       <Image
         className="relative rounded-full h-32 w-32 mx-auto object-cover"
@@ -26,14 +27,29 @@ const Hero = (props: Props) => {
         width={128}
         height={128}
       />
-      <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[1rem]">Full Stack Developer</h2>
-      <h1 className="text-3xl lg:text-4xl font-semibold px-10 z-10">
-        {'<'}
-        <span className="mr-2">{text}</span>
-        <Cursor cursorColor="#f7ab0a"/>
-        {'/>'}
-      </h1>
-      
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[1rem]">Full Stack Developer</h2>
+        <h1 className="text-3xl lg:text-4xl font-semibold px-10">
+          {'<'}
+          <span className="mr-2">{text}</span>
+          <Cursor cursorColor="#f7ab0a"/>
+          {'/>'}
+        </h1>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton" type="button">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton" type="button">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton" type="button">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton" type="button">Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
