66b0542b759eeafcbab0d44e649ab586e350b63f
David Vera Castillo <indigodavid@gmail.com>
2022-11-12 22:20:37 -0500

    🛠 Refactor About heading using sectionHeader class

--- components/About.tsx
+++ components/About.tsx
@@ -9,9 +9,9 @@ const About = (props: Props) => {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className="flex flex-col relative h-screen text-center md:text-justify md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+      className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
     >
-      <h3 className="absolute top-24 uppercase tracking-[1.25rem] text-gray-500 text-2xl">
+      <h3 className="sectionHeader">
         About
       </h3>
       <motion.img
