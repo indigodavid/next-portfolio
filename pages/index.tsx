8b6530b901978145feff4b3a2697d63d30e63d7a
David Vera Castillo <indigodavid@gmail.com>
2022-11-12 22:22:15 -0500

    ✅ Add new section and fix snap behavior

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,20 @@ import Image from 'next/image'
 import About from '../components/About'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import WorkExperience from '../components/WorkExperience'
 
 export default function Home() {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>David Vera&apos;s Next Portfolio</title>
-        <meta name="description" content="Software Developer portfolio using React" />
+        <meta name="description" content="Software Developer portfolio using NextJS" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
       {/* Hero */}
-      <section id="hero" className="snap-center">
+      <section id="hero" className="snap-start">
         <Hero />
       </section>
 
@@ -25,7 +26,9 @@ export default function Home() {
       </section>
 
       {/* Experience */}
-
+      <section id="experience" className="snap-center">
+        <WorkExperience />
+      </section>
       {/* Skills */}
 
       {/* Projects */}
