ccf9b5ebce1359cb943a148a2c2b2d90b85b8ed6
David Vera Castillo <indigodavid@gmail.com>
2022-11-13 21:29:16 -0500

    ✅ Add Projects component

---
+++ components/Projects.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+type Props = {}
+
+const Projects = (props: Props) => {
+  const projects = [1, 2, 3, 4, 5];
+  return (
+    <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
+      <h3 className="sectionHeader">
+        Projects
+      </h3>
+      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+        {projects.map((project, i) => (
+          <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1.5 }}
+            key={i}
+            className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen"
+          >
+            <motion.img
+              initial={{
+                y: -300,
+                opacity: 0,
+              }}
+              transition={{ duration: 1.2 }}
+              whileInView={{
+                opacity: 1,
+                y: 0,
+              }}
+              viewport={{ once: true }}
+              src="https://ik.imagekit.io/ezcar/ezcar-thumbnail_9cWc3GjJg.png"
+              alt="Project"
+            />
+            <div className="space-y-10 px-0 md:px-10 max-w-6xl">
+              <h4 className="text-4xl font-semibold text-center">
+                <span className="underline decoration-[#f7ab0a]/50">
+                  Case Study {i + 1} of {projects.length}:
+                </span>{' '}
+                {project}
+              </h4>
+              <p className="text-lg text-center md:text-left">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium aspernatur explicabo velit necessitatibus porro corporis ab totam obcaecati cupiditate voluptas.
+              </p>
+            </div>
+          </motion.div>
+        ))}
+
+        
+      </div>
+      <div className="w-full absolute top-[30%] bg-[#f7ab0a]/10 left-0 h-[31em] -skew-y-12" />
+    </div>
+  )
+}
+
+export default Projects
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import About from '../components/About'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import Projects from '../components/Projects'
 import Skills from '../components/Skills'
 import WorkExperience from '../components/WorkExperience'
 
@@ -37,6 +38,9 @@ export default function Home() {
       </section>
 
       {/* Projects */}
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
 
       {/* Contact Me */}
 
