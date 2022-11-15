07a4a99407cedf26e3984296e213c9d413226fac
David Vera Castillo <indigodavid@gmail.com>
2022-11-15 15:00:29 -0500

    ✅ Add back to top link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import About from '../components/About'
 import ContactMe from '../components/ContactMe'
 import Header from '../components/Header'
@@ -47,6 +48,20 @@ export default function Home() {
       <section id="contact" className="snap-start">
         <ContactMe />
       </section>
+
+      <Link href="#hero">
+        <footer className="sticky bottom-5 w-full cursor-pointer">
+          <div className="flex items-center justify-center">
+            <Image
+              className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
+              src="https://ik.imagekit.io/ezcar/DavidVeraNov2022_KDbSgVGgn.jpg"
+              alt="Up"
+              width={40}
+              height={40}
+            />
+          </div>
+        </footer>
+      </Link>
 
     </div>
   )
