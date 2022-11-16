1ec477c0468f76978b48c9ddabeb848cfcbf944d
David Vera Castillo <indigodavid@gmail.com>
2022-11-16 15:54:41 -0500

    ✅ Append all schemas to Sanity main schema

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -5,10 +5,11 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // We import object and document schemas
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+import pageInfo from './pageInfo'
+import experience from './experience'
+import project from './project'
+import skill from './skill'
+import social from './social'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -19,11 +20,10 @@ export default createSchema({
   types: schemaTypes.concat([
     // The following are document types which will appear
     // in the studio.
-    post,
-    author,
-    category,
-    // When added to this list, object types can be used as
-    // { type: 'typename' } in other document schemas
-    blockContent,
+    pageInfo,
+    skill,
+    experience,
+    social,
+    project,
   ]),
 })
