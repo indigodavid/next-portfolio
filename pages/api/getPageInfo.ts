a8aef6c67dd38f788fc28a16918015c61cc16bd9
David Vera Castillo <indigodavid@gmail.com>
2022-11-23 10:42:37 -0500

    🛠 Fix pageInfo query

--- pages/api/getPageInfo.ts
+++ pages/api/getPageInfo.ts
@@ -4,7 +4,7 @@ import { sanityClient } from '../../sanity';
 import { PageInfo } from "../../typings";
 
 const query = groq`
-  *[_type == "pageInfo"][]
+  *[_type == "pageInfo"][0]
 `;
 
 type Data = {
