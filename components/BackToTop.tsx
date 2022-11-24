fb11c5e5060aca6c83252af96f59a0173a4b8d98
David Vera Castillo <indigodavid@gmail.com>
2022-11-23 23:38:49 -0500

    ✅ Edit BackToTop image

--- components/BackToTop.tsx
+++ components/BackToTop.tsx
@@ -10,8 +10,8 @@ const BackToTop = (props: Props) => {
       <footer className="sticky bottom-5 w-full cursor-pointer">
         <div className="flex items-center ml-5">
           <Image
-            className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
-            src="https://ik.imagekit.io/ezcar/DavidVeraNov2022_KDbSgVGgn.jpg"
+            className="h-10 w-10 bg-[#f7ab0a] rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
+            src="https://ik.imagekit.io/ezcar/Hands-One-Finger-icon_KDPHQ0LRz.png"
             alt="Up"
             width={40}
             height={40}
