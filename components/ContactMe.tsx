4c862bb5e3799b2c290cc432204182ada449e8dc
David Vera Castillo <indigodavid@gmail.com>
2022-11-23 23:16:03 -0500

    ✅ Add dynamic data to ContactMe component

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { EnvelopeIcon, MapPinIcon, PhoneIcon } from '@heroicons/react/24/solid'
-import { useForm, SubmitHandler } from "react-hook-form";
+import { PageInfo } from '../typings';
+import Link from 'next/link';
 
 type Inputs = {
   name: string,
@@ -9,65 +10,66 @@ type Inputs = {
   message: string,
 };
 
-type Props = {}
+type Props = {
+  pageInfo: PageInfo;
+}
 
-const ContactMe = (props: Props) => {
-  const { register, handleSubmit } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = formData => {
-    window.location.href = `mailto:david.verac@hotmail.com?subject=${formData.subject}&body=Hi, my name is ${formData.name}. ${formData.message} (${formData.email})`
-  };
-
+const ContactMe = ({ pageInfo }: Props) => {
+  
   return (
-    <div className="h-screen relative flex flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+    <div className="h-screen relative flex flex-col pt-28 sm:pt-0 text-left md:flex-row w-full max-w-7xl md:px-10 justify-evenly mx-auto items-center">
       <h3 className="sectionHeader">Contact</h3>
-      <div className="flex flex-col space-y-10">
-        <h4 className="text-4xl font-semibold text-center">
-          I have got just what you need.{' '}
-          <span className="underline decoration-[#f7ab0a]/50">Lets Talk.</span>
+      <div className="flex flex-col md:space-y-5 w-full items-center">
+        <h4 className="text-lg sm:text-2xl font-semibold text-center">
+          I am always interested in new projects.{' '}
+          <span className="underline decoration-[#f7ab0a]/50">Let&apos;s Talk.</span>
         </h4>
-        <div className="space-y-10">
-          <div className="flex items-center space-x-5 justify-center">
-            <PhoneIcon className="text-[#f7ab0a] h-7 w-7 animate-pulse" />
-            <p>+593986999845</p>
-          </div>
+        <div>
+          <Link href={`sms:${pageInfo.phoneNumber}`}>
+            <div className="flex items-center space-x-5 ml-2">
+              <PhoneIcon className="text-[#f7ab0a] h-7 w-7 animate-pulse" />
+              <p>{pageInfo.phoneNumber}</p>
+            </div>
+          </Link>
         </div>
 
         <div>
-          <div className="flex items-center space-x-5 justify-center">
-            <EnvelopeIcon className="text-[#f7ab0a] h-7 w-7 animate-pulse" />
-            <p>david.verac@hotmail.com</p>
-          </div>
+          <Link href={`mailto:${pageInfo.email}`}>
+            <div className="flex items-center space-x-5 ml-2">
+              <EnvelopeIcon className="text-[#f7ab0a] h-7 w-7 animate-pulse" />
+              <p>{pageInfo.email}</p>
+            </div>
+          </Link>
         </div>
 
         <div>
-          <div className="flex items-center space-x-5 justify-center">
+          <div className="flex items-center space-x-5 ml-2">
             <MapPinIcon className="text-[#f7ab0a] h-7 w-7 animate-pulse" />
-            <p>N7D SN LT 123, Pintag, Quito, Ecuador</p>
+            <p>{pageInfo.address}</p>
           </div>
         </div>
 
-        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col space-y-2 w-fit mx-auto">
-          <div className="flex space-x-2">
+        <form action={pageInfo.formHandler} method="POST" className="flex flex-col space-y-2 w-fit mx-auto">
+          <div className="flex flex-col gap-2 sm:flex-row">
             <input 
-              {...register('name')}
               placeholder="Name"
               className="contactInput"
               type="text"
               name="name"
               id="name"
+              required
             />
             <input
-              {...register('email')}
               placeholder="Email"
               className="contactInput"
               type="email"
               name="email"
               id="email"
+              required
             />
           </div>
 
           <input
-            {...register('subject')}
             placeholder="Subject"
             className="contactInput"
             type="text"
@@ -76,13 +78,14 @@ const ContactMe = (props: Props) => {
           />
 
           <textarea
-            {...register('message')}
             placeholder="Message"
             className="contactInput"
             name="message"
-            id="message" />
+            id="message"
+            required
+          />
           <button
-            className="bg-[#f7ab0a] py-5 px-10 rounded-md text-black font-bold text-lg"
+            className="bg-[#f7ab0a] py-4 px-10 rounded-md text-black font-bold text-lg"
             type="submit"
           >
             Submit
--- typings.d.ts
+++ typings.d.ts
@@ -53,6 +53,7 @@ export interface PageInfo extends SanityBody {
   name: string;
   phoneNumber: string;
   profilePic: Image;
+  formHandler: string;
 }
 
 export interface Experience extends SanityBody {
