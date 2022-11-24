79dee69f0af3f622e229d294e9913b81661085d4
David Vera Castillo <indigodavid@gmail.com>
2022-11-23 23:17:19 -0500

    🛠 Fix ContactMe and BackToTop implementations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
 import About from '../components/About'
+import BackToTop from '../components/BackToTop'
 import ContactMe from '../components/ContactMe'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
@@ -30,7 +29,7 @@ export default function Home(
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80">
       <Head>
-        <title>David Vera&apos;s Next Portfolio</title>
+        <title>{pageInfo.name}&apos;s Next Portfolio</title>
         <meta name="description" content="Software Developer portfolio using NextJS" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -63,22 +62,10 @@ export default function Home(
 
       {/* Contact Me */}
       <section id="contact" className="snap-start">
-        <ContactMe />
+        <ContactMe pageInfo={pageInfo} />
       </section>
 
-      <Link href="#hero">
-        <footer className="sticky bottom-5 w-full cursor-pointer">
-          <div className="flex items-center justify-center">
-            <Image
-              className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
-              src="https://ik.imagekit.io/ezcar/DavidVeraNov2022_KDbSgVGgn.jpg"
-              alt="Up"
-              width={40}
-              height={40}
-            />
-          </div>
-        </footer>
-      </Link>
+      <BackToTop />
 
     </div>
   )
