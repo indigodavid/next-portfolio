b7e883b2fc79d4104efa508aa972e83339d7b8f4
David Vera Castillo <indigodavid@gmail.com>
2022-11-30 23:22:15 -0500

    ✅ Use accent color on BackgroundCircles component

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -21,7 +21,7 @@ const BackgroundCircles = (props: Props) => (
     <div className="absolute border border-[#333] rounded-full h-[200px] w-[200px] mt-52 animate-ping" />
     <div className="absolute border border-[#333] rounded-full h-[300px] w-[300px] mt-52" />
     <div className="absolute border border-[#333] rounded-full h-[500px] w-[500px] mt-52" />
-    <div className="absolute border border-[#f7ab0a] rounded-full opacity-20 h-[650px] w-[650px] mt-52 animate-pulse" />
+    <div className="absolute border border-accent rounded-full opacity-20 h-[650px] w-[650px] mt-52 animate-pulse" />
     <div className="absolute border border-[#333] rounded-full h-[800px] w-[800px] mt-52" />
   </motion.div>
 );
