46962bbcb249d3dc3bfd27a31507c4e361bc24bf
David Vera Castillo <indigodavid@gmail.com>
2022-11-30 23:22:25 -0500

    ✅ Use accent color on BackToTop component

--- components/BackToTop.tsx
+++ components/BackToTop.tsx
@@ -9,7 +9,7 @@ const BackToTop = (props: Props) => (
     <footer className="sticky bottom-5 w-full cursor-pointer">
       <div className="flex items-center ml-5">
         <Image
-          className="h-10 w-10 bg-[#f7ab0a] rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
+          className="h-10 w-10 bg-accent rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
           src="https://ik.imagekit.io/ezcar/Hands-One-Finger-icon_KDPHQ0LRz.png"
           alt="Up"
           width={40}
