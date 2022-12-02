da6de77a30364442d033ef30223209f5b98aafdd
David Vera Castillo <indigodavid@gmail.com>
2022-12-02 17:05:55 -0500

    🛠 Fix Skills layout

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -24,9 +24,9 @@ const Skill = ({ skill, directionLeft }: Props) => (
       }}
       src={urlFor(skill?.image).url()}
       alt={skill?.title}
-      className="rounded-lg bg-white border border-gray-500 object-contain w-12 h-12 md:w-24 md:h-24 filter group-hover:grayscale transition duration-300 ease-in-out"
+      className="rounded-lg bg-white border border-gray-500 object-contain w-12 h-12 md:w-16 md:h-16 filter group-hover:grayscale transition duration-300 ease-in-out"
     />
-    <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-slate-700 w-16 h-16 md:w-24 md:h-24 rounded-lg z-0">
+    <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-slate-700 w-12 h-12 md:w-16 md:h-16 rounded-lg z-0">
       <div className="flex items-center justify-center h-full">
         <p className="text-sm sm:text-lg font-bold text-white opacity-100">{skill?.title}</p>
       </div>
