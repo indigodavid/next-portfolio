bf6df469c3afec659af6b80093160ae0434caab4
David Vera Castillo <indigodavid@gmail.com>
2022-12-02 13:30:09 -0500

    🛠 Fix contrast errors

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -32,7 +32,7 @@ const Hero = ({ pageInfo }: Props) => {
         height={128}
       />
       <div className="z-20">
-        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[1rem]">
+        <h2 className="text-sm uppercase text-gray-300 pb-2 tracking-[1rem]">
           {pageInfo?.role ? pageInfo?.role : 'Software Developer' }
         </h2>
         <h1 className="text-3xl lg:text-4xl font-semibold px-10">
