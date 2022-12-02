ea8a9d8733c8ec801488fb7fe61bf60a93074ee4
David Vera Castillo <indigodavid@gmail.com>
2022-12-02 17:06:40 -0500

    🛠 Replace BackToTop Icon

--- components/BackToTop.tsx
+++ components/BackToTop.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import Image from 'next/image';
+import { HomeIcon } from '@heroicons/react/24/solid';
 import React from 'react';
 
 type Props = {}
@@ -8,13 +8,7 @@ const BackToTop = (props: Props) => (
   <Link href="#hero">
     <footer className="sticky bottom-5 w-full cursor-pointer">
       <div className="flex items-center ml-5">
-        <Image
-          className="h-10 w-10 bg-accent rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
-          src="https://ik.imagekit.io/ezcar/Hands-One-Finger-icon_KDPHQ0LRz.png"
-          alt="Up"
-          width={40}
-          height={40}
-        />
+        <HomeIcon className="h-10 w-10 bg-accent rounded-full filter grayscale hover:grayscale-0 cursor-pointer text-black"/>
       </div>
     </footer>
   </Link>
